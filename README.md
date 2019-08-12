b29b11032dc09eeb2d4ed6897708b3ea3b0441ab
prerna <chaturvediprerna03@gmail.com>
2019-08-12 14:24:30 +1200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-Simple React App  Count Star
-
-Download App ..
-
-open folder in git bash
-
-run npm install command, to install all the modules
-
-run npm start to run the app
---
+++ precache-manifest.b0e27dbe1f6951ab8429b5782297047c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015ce808d53fc232fe37b05302a714c9",
+    "url": "/count_star/index.html"
+  },
+  {
+    "revision": "3175d51874f7c1a92d8e",
+    "url": "/count_star/static/css/main.7296d573.chunk.css"
+  },
+  {
+    "revision": "f474dd0db344b665e7f0",
+    "url": "/count_star/static/js/2.94d64232.chunk.js"
+  },
+  {
+    "revision": "3175d51874f7c1a92d8e",
+    "url": "/count_star/static/js/main.dde584a8.chunk.js"
+  },
+  {
+    "revision": "d1ff62aefa01d1067112",
+    "url": "/count_star/static/js/runtime~main.7ec8de9b.js"
+  }
+]);
